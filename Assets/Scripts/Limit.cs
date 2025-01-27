95a32655b22c8fa5bf336baa8c9c7ac1c20d9b8e
Muse BOUCHARD <muse.bouchard@isen.yncrea.fr>
2025-01-27 18:39:56 +0100

    [FEATURE] Advanced map generation

--- Assets/Scripts/Limit.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Limit : MonoBehaviour
-{
-    private void OnTriggerEnter(Collider other) => Destroy(other.gameObject);
-}
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,26 +5,24 @@ public class MapGenerator : MonoBehaviour
 {
     [SerializeField] private FloatSO speed;
     [SerializeField] private GameObject mapBlock;
+    [SerializeField] private List<GameObject> mapBlocks = new List<GameObject>(6);
 
     // Start is called before the first frame update
     void Start()
     {
         speed.value = 1f;
+        int blockType = Random.Range(0, 6);
 
-        Instantiate(mapBlock);
-        Instantiate(mapBlock, new Vector3(0, 0, 11), Quaternion.Euler(0, 180, 0));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Instantiate(mapBlock, new Vector3(0, 0, 0), Quaternion.Euler(0, 180, 0));
+        Instantiate(mapBlocks[blockType], new Vector3(0, 0, 11), Quaternion.Euler(0, 180, 0));
     }
 
     private void OnTriggerEnter(Collider other)
-    {
+    { 
+        int blockType = Random.Range(0, 6);
         float diff = 11 + other.transform.position.z;
-        Instantiate(mapBlock, new Vector3(0, 0, 11 + diff), Quaternion.Euler(0, 180, 0));
+
+        Instantiate(mapBlocks[blockType], new Vector3(0, 0, 11 + diff), Quaternion.Euler(0, 180, 0));
         Destroy(other.gameObject);
     }
 }
