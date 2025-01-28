fc23005b622a02b77ec60fc4338321576ad08145
Muse BOUCHARD <muse.bouchard@isen.yncrea.fr>
2025-01-28 15:25:24 +0100

    [FEATURE] Added time based timer

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     [SerializeField] private FloatSO speed;
+    [SerializeField] private UintSO score;
     [SerializeField] private GameObject mapBlock;
     [SerializeField] private List<GameObject> mapBlocks = new List<GameObject>(6);
+    private float timer = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         speed.value = 5f;
+        score.value = 0;
 
         Instantiate(mapBlock, new Vector3(0, 0, -11), Quaternion.Euler(0, 180, 0));
         Instantiate(mapBlock, new Vector3(0, 0, 0), Quaternion.Euler(0, 180, 0));
         Instantiate(mapBlocks[Random.Range(0, 6)], new Vector3(0, 0, 11), Quaternion.Euler(0, 180, 0));
         Instantiate(mapBlocks[Random.Range(0, 6)], new Vector3(0, 0, 22), Quaternion.Euler(0, 180, 0));
+    }
+
+    void Update()
+    {
+        if(speed.value > 0)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > 5f)
+            {
+                score.value += 100;
+                timer = 0;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -25,6 +42,7 @@ public class MapGenerator : MonoBehaviour
 
         Instantiate(mapBlocks[blockType], new Vector3(0, 0, 22 + diff), Quaternion.Euler(0, 180, 0));
         Destroy(other.gameObject);
+
         speed.value += .1f;
     }
 }
