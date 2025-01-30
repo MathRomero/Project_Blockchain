486a27a5c1d326e632775597f9f2507c83740e57
Muse BOUCHARD <muse.bouchard@isen.yncrea.fr>
2025-01-30 19:20:43 +0100

    [FEATURE] Added GameOver screen

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -1,27 +1,40 @@
-
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Death : MonoBehaviour
 {
     Animator animator;
     [SerializeField] private FloatSO speed;
-    // Start is called before the first frame update
+    [SerializeField] private Object gameOverScene;
+    
+    private float timer = 0f;
+    private bool isDead = false;
+
     void Start()
     {
         animator = GetComponent<Animator>();
     }
+
+    private void Update()
+    {
+        if(isDead)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > 2f)
+                SceneManager.LoadScene(gameOverScene.name);
+        }
+            
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Obstacles"))
         {
             animator.SetTrigger("Dead");
             speed.value = 0f;
-        }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            isDead = true;
+        }
     }
 }
